8dc80d9d8dd9bffc1343d309739c84435a252afe
Riyadulislamratul <riadul1810833319@gmail.com>
2026-08-11 23:49:59 +0600

    Hero section error fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,16 +9,16 @@ const Hero = () => {
       <div className="relative min-h-[500px] lg:min-h-[520px]">
 
         {/* Hero Content */}
-        <div className="relative z-10 flex py-65 flex-col justify-between ">
+        <div className="relative z-10 flex py-6 flex-col justify-between ">
 
           {/* Heading */}
           <div className="px-8 pt-10 sm:px-12 sm:pt-12 lg:w-[52%] lg:px-20 lg:pt-12">
             <h1 className="font-light leading-[0.95]">
-              <span className="block text-4xl text-[#003478] sm:text-5xl lg:text-6xl">
+              <span className="block text-6xl text-[#003478] sm:text-5xl lg:text-6xl">
                 THE FUTURE OF
               </span>
 
-              <span className="block text-4xl font-medium text-[#f5a000] sm:text-5xl lg:text-6xl">
+              <span className="block text-6xl font-medium text-[#f5a000] sm:text-5xl lg:text-6xl mt-5">
                 EXHILARATION
               </span>
             </h1>
@@ -27,8 +27,8 @@ const Hero = () => {
           {/* Mustang Title Bars */}
           <div className="absolute left-0 top-[155px] w-full lg:top-[156px]">
 
-            <div className="flex h-[70px] items-center bg-[#292d2d] px-8 sm:px-12 lg:px-20 absolute left-[-193px] top-[104px] ">
-              <h2 className="text-2xl font-light tracking-wide text-white sm:text-3xl lg:text-4xl">
+            <div className="flex h-[70px] items-center bg-[#292d2d] w-full absolute left-[-387px] top-[117px] ">
+              <h2 className="text-2xl font-light tracking-wide text-white sm:text-3xl lg:text-6xl pl-120">
                 FORD MUSTANG
                 <span className="ml-2 text-xl sm:text-2xl lg:text-3xl">
                   2021
@@ -36,19 +36,19 @@ const Hero = () => {
               </h2>
             </div>
 
-            <div className="mt-3 h-[70px] bg-[#292d2d] absolute left-0 top-0 w-full" />
+            <div className="mt-3 h-[70px] bg-[#292d2d] absolute -left-[385px] top-[195px] w-full" />
           </div>
 
           {/* Buttons */}
-          <div className="mb-24 flex items-center gap-6 px-8 sm:px-12 lg:mb-20 lg:px-20">
+          <div className="mb-24 flex items-center gap-6 px-8 sm:px-12 lg:mb-20 lg:px-20 mt-70">
 
             {/* Build & Price */}
-            <button className="bg-[#003478] px-7 py-4 text-sm font-medium text-white transition hover:bg-[#00285c]">
+            <button className="bg-[#003478] px-7 py-4 text-lg font-medium text-white transition hover:bg-[#00285c]">
               Build & Price
             </button>
 
             {/* Watch Video */}
-            <button className="flex items-center gap-4 text-sm text-[#111] transition hover:text-[#003478]">
+            <button className="flex items-center gap-4 text-lg text-[#111] transition hover:text-[#003478]">
               <span className="flex h-8 w-8 items-center justify-center rounded-full bg-[#003478] text-white">
                 <Play size={12} fill="currentColor" />
               </span>
@@ -87,7 +87,7 @@ const Hero = () => {
       </div>
 
       {/* Bottom CTA */}
-      <div className="flex h-[55px] items-center justify-center bg-[#292d2d]">
+      <div className="flex h-[55px] items-center justify-center bg-[#292d2d] mt-25">
         <button className="text-xl font-light tracking-wide text-white transition hover:text-gray-300 sm:text-2xl">
           Custom Order Your Mustang
         </button>
